f9e43e6a811575e60c7f7a2cac8933cf9d9ff1df
Mitchell D. Johnson <johnson.mitchelld@gmail.com>
2020-12-14 21:26:42 -0800

    Add ephemeris unzip capability.

--- get_ephemeris.py
+++ get_ephemeris.py
@@ -1,5 +1,14 @@
 from ftplib import FTP_TLS
+import gzip
+import shutil
 ftps = ftps = FTP_TLS('gdc.cddis.eosdis.nasa.gov')
 ftps.login()
 ftps.prot_p()
-print(ftps.nlst())
+print(ftps.nlst('gnss/data/daily/2020/brdc'))
+filename = 'BRDC00IGS_R_20200010000_01D_MN.rnx.gz'
+with open(filename, 'wb') as handle:
+    ftps.retrbinary('RETR gnss/data/daily/2020/brdc/BRDC00IGS_R_20200010000_01D_MN.rnx.gz', handle.write)
+
+with gzip.open('BRDC00IGS_R_20200010000_01D_MN.rnx.gz', 'rb') as f_in:
+    with open('BRDC00IGS_R_20200010000_01D_MN.rnx', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
